e5b1a9acc3fab818613f0d3766bd5be5ca2ddc53
legarrec <legarrec.vincent@gmail.com>
2015-01-28 22:14:03 +0100

    Add tests for package with buildtree.

---
+++ tests/PackageClasse3-b.java
@@ -0,0 +1,7 @@
+package package.pack2.pack3;
+
+/**
+ * Classe 3
+*/
+public class PackageClasse3 {
+}
--- tests/tests.sh
+++ tests/tests.sh
@@ -866,249 +866,249 @@ if [[ -s result.txt ]] ; then
 exit 1
 fi ;
 
-#rm -Rf result/*
-#../src/dia2code -t java package.dia -d result --buildtree || exit 1
-#if [[ ! -a result/package/PackageClasse1.hpp ]] ; then
-#exit 1
-#fi ;
-#if [[ ! -a result/package/pack2/PackageClasse2.hpp ]] ; then
-#exit 1
-#fi ;
-#if [[ ! -a result/package/pack2/pack3/PackageClasse3.hpp ]] ; then
-#exit 1
-#fi ;
-#diff -pu PackageClasse1-b.hpp result/package/PackageClasse1.hpp > result.txt
-#if [[ -s result.txt ]] ; then
-#exit 1
-#fi ;
-#diff -pu PackageClasse2-b.hpp result/package/pack2/PackageClasse2.hpp > result.txt
-#if [[ -s result.txt ]] ; then
-#exit 1
-#fi ;
-#diff -pu PackageClasse3-b.hpp result/package/pack2/pack3/PackageClasse3.hpp > result.txt
-#if [[ -s result.txt ]] ; then
-#exit 1
-#fi ;
-#
+rm -Rf result/*
+../src/dia2code -t java package.dia -d result --buildtree || exit 1
+if [[ ! -a result/package/PackageClasse1.java ]] ; then
+exit 1
+fi ;
+if [[ ! -a result/package/pack2/PackageClasse2.java ]] ; then
+exit 1
+fi ;
+if [[ ! -a result/package/pack2/pack3/PackageClasse3.java ]] ; then
+exit 1
+fi ;
+diff -pu PackageClasse1-b.java result/package/PackageClasse1.java > result.txt
+if [[ -s result.txt ]] ; then
+exit 1
+fi ;
+diff -pu PackageClasse2-b.java result/package/pack2/PackageClasse2.java > result.txt
+if [[ -s result.txt ]] ; then
+exit 1
+fi ;
+diff -pu PackageClasse3-b.java result/package/pack2/pack3/PackageClasse3.java > result.txt
+if [[ -s result.txt ]] ; then
+exit 1
+fi ;
+
 #rm -Rf result/*
 #../src/dia2code -t java enum.dia -d result || exit 1
-#if [[ ! -a result/enumeration.hpp ]] ; then
+#if [[ ! -a result/enumeration.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu enumeration-11.hpp result/enumeration.hpp > result.txt
+#diff -pu enumeration-11.java result/enumeration.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java operations.dia -d result || exit 1
-#if [[ ! -a result/operations.hpp ]] ; then
+#if [[ ! -a result/operations.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu operations-11.hpp result/operations.hpp > result.txt
+#diff -pu operations-11.java result/operations.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
-#rm result/operations.hpp
+#rm result/operations.java
 #../src/dia2code -t java operations.dia -d result -l COPYING || exit 1
-#if [[ ! -a result/operations.hpp ]] ; then
+#if [[ ! -a result/operations.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu operations_COPYING-11.hpp result/operations.hpp > result.txt
+#diff -pu operations_COPYING-11.java result/operations.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java attributes.dia -d result || exit 1
-#if [[ ! -a result/attributes.hpp ]] ; then
+#if [[ ! -a result/attributes.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu attributes.hpp result/attributes.hpp > result.txt
+#diff -pu attributes.java result/attributes.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java dependances.dia -d result || exit 1
-#if [[ ! -a result/dependances.hpp ]] ; then
+#if [[ ! -a result/dependances.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu dependances.hpp result/dependances.hpp > result.txt
+#diff -pu dependances.java result/dependances.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #rm -Rf result/*
 #../src/dia2code -t java dependances_pack.dia -d result || exit 1
-#if [[ ! -a result/DepClassePack1.hpp ]] ; then
+#if [[ ! -a result/DepClassePack1.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack2.hpp ]] ; then
+#if [[ ! -a result/DepClassePack2.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack3.hpp ]] ; then
+#if [[ ! -a result/DepClassePack3.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack4.hpp ]] ; then
+#if [[ ! -a result/DepClassePack4.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepPack.hpp ]] ; then
+#if [[ ! -a result/DepPack.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepPack.hpp result/DepPack.hpp > result.txt
+#diff -pu DepPack.java result/DepPack.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack1.hpp result/DepClassePack1.hpp > result.txt
+#diff -pu DepClassePack1.java result/DepClassePack1.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack2.hpp result/DepClassePack2.hpp > result.txt
+#diff -pu DepClassePack2.java result/DepClassePack2.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack3.hpp result/DepClassePack3.hpp > result.txt
+#diff -pu DepClassePack3.java result/DepClassePack3.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack4.hpp result/DepClassePack4.hpp > result.txt
+#diff -pu DepClassePack4.java result/DepClassePack4.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #rm -Rf result/*
 #../src/dia2code -t java dependances_pack.dia -d result --buildtree || exit 1
-#if [[ ! -a result/DepClassePack1.hpp ]] ; then
+#if [[ ! -a result/DepClassePack1.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack2.hpp ]] ; then
+#if [[ ! -a result/DepClassePack2.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack3.hpp ]] ; then
+#if [[ ! -a result/DepClassePack3.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepClassePack4.hpp ]] ; then
+#if [[ ! -a result/DepClassePack4.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepPack/DepClasse1.hpp ]] ; then
+#if [[ ! -a result/DepPack/DepClasse1.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/DepPack/pack2/DepClasse2.hpp ]] ; then
+#if [[ ! -a result/DepPack/pack2/DepClasse2.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClasse1-b.hpp result/DepPack/DepClasse1.hpp > result.txt
+#diff -pu DepClasse1-b.java result/DepPack/DepClasse1.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClasse2-b.hpp result/DepPack/pack2/DepClasse2.hpp > result.txt
+#diff -pu DepClasse2-b.java result/DepPack/pack2/DepClasse2.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack1-b.hpp result/DepClassePack1.hpp > result.txt
+#diff -pu DepClassePack1-b.java result/DepClassePack1.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack2-b.hpp result/DepClassePack2.hpp > result.txt
+#diff -pu DepClassePack2-b.java result/DepClassePack2.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack3-b.hpp result/DepClassePack3.hpp > result.txt
+#diff -pu DepClassePack3-b.java result/DepClassePack3.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu DepClassePack4-b.hpp result/DepClassePack4.hpp > result.txt
+#diff -pu DepClassePack4-b.java result/DepClassePack4.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java structure.dia -d result || exit 1
-#if [[ ! -a result/structure.hpp ]] ; then
+#if [[ ! -a result/structure.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu structure.hpp result/structure.hpp > result.txt
+#diff -pu structure.java result/structure.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #rm -Rf result/*
 #../src/dia2code -t java inherence.dia -d result || exit 1
-#if [[ ! -a result/InherenceClasseChildren.hpp ]] ; then
+#if [[ ! -a result/InherenceClasseChildren.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/InherenceClasseParent.hpp ]] ; then
+#if [[ ! -a result/InherenceClasseParent.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/InherencePack1.hpp ]] ; then
+#if [[ ! -a result/InherencePack1.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherenceClasseChildren.hpp result/InherenceClasseChildren.hpp > result.txt
+#diff -pu InherenceClasseChildren.java result/InherenceClasseChildren.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherenceClasseParent.hpp result/InherenceClasseParent.hpp > result.txt
+#diff -pu InherenceClasseParent.java result/InherenceClasseParent.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherencePack1.hpp result/InherencePack1.hpp > result.txt
+#diff -pu InherencePack1.java result/InherencePack1.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #rm -Rf result/*
 #../src/dia2code -t java inherence.dia -d result --buildtree || exit 1
-#if [[ ! -a result/InherenceClasseChildren.hpp ]] ; then
+#if [[ ! -a result/InherenceClasseChildren.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/InherenceClasseParent.hpp ]] ; then
+#if [[ ! -a result/InherenceClasseParent.java ]] ; then
 #exit 1
 #fi ;
-#if [[ ! -a result/InherencePack1/InherenceClasseParent2.hpp ]] ; then
+#if [[ ! -a result/InherencePack1/InherenceClasseParent2.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherenceClasseChildren-b.hpp result/InherenceClasseChildren.hpp > result.txt
+#diff -pu InherenceClasseChildren-b.java result/InherenceClasseChildren.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherenceClasseParent-b.hpp result/InherenceClasseParent.hpp > result.txt
+#diff -pu InherenceClasseParent-b.java result/InherenceClasseParent.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
-#diff -pu InherenceClasseParent2-b.hpp result/InherencePack1/InherenceClasseParent2.hpp > result.txt
+#diff -pu InherenceClasseParent2-b.java result/InherencePack1/InherenceClasseParent2.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java typedef.dia -d result || exit 1
-#if [[ ! -a result/TypeDef.hpp ]] ; then
+#if [[ ! -a result/TypeDef.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu TypeDef.hpp result/TypeDef.hpp > result.txt
+#diff -pu TypeDef.java result/TypeDef.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java getset.dia -d result || exit 1
-#if [[ ! -a result/GetSet.hpp ]] ; then
+#if [[ ! -a result/GetSet.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu GetSet-11.hpp result/GetSet.hpp > result.txt
+#diff -pu GetSet-11.java result/GetSet.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java constance.dia -d result || exit 1
-#if [[ ! -a result/constance.hpp ]] ; then
+#if [[ ! -a result/constance.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu constance-11.hpp result/constance.hpp > result.txt
+#diff -pu constance-11.java result/constance.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
 #
 #../src/dia2code -t java template.dia -d result || exit 1
-#if [[ ! -a result/Template.hpp ]] ; then
+#if [[ ! -a result/Template.java ]] ; then
 #exit 1
 #fi ;
-#diff -pu Template.hpp result/Template.hpp > result.txt
+#diff -pu Template.java result/Template.java > result.txt
 #if [[ -s result.txt ]] ; then
 #exit 1
 #fi ;
