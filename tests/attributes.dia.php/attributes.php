a4697092835c9b711260dd40b44e3e322ec79d97
legarrec <legarrec.vincent@gmail.com>
2015-02-25 11:24:11 +0100

    Make PHP pass attributes.dia.

---
+++ tests/attributes.dia.php/attributes.php
@@ -0,0 +1,11 @@
+<?php
+class attributes {
+    // Attributes
+    /** @var type1 com1 */
+    public $nom1 = val1;
+    /** @var type2 com2 */
+    private static $nom2 = val2;
+    /** @var type3 com3 */
+    protected $nom3 = val3;
+}
+?>
